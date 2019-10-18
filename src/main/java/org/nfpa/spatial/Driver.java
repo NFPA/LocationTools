cbd2411a5a81ce8bd1448a040239842033282cfd
rahul-pande <93rahulpande@gmail.com>
2019-10-18 17:52:51 +0000

    added logging

--- src/main/java/org/nfpa/spatial/Driver.java
+++ src/main/java/org/nfpa/spatial/Driver.java
@@ -1,35 +1,38 @@
 package org.nfpa.spatial;
 
+import org.apache.log4j.Logger;
 import org.locationtech.jts.io.ParseException;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 
 public class Driver {
+    private static Logger logger = Logger.getLogger("Driver");
     public static void main(String... args) throws IOException, IllegalAccessException, InvocationTargetException, ParseException, org.json.simple.parser.ParseException {
+//        System.setProperty("java.library.path", "/home/ubuntu/jpostal/src/main/jniLibs");
         String option = args[0];
         switch (option){
             case "--process":{
-                System.out.println("Processing");
+                logger.info("Processing raw TIGER data.");
                 new TigerProcessor().main(new String[] {args[1]});
             }
             break;
             case "--index":{
-                System.out.println("Indexing");
+                logger.info("Indexing.");
                 new TigerIndexer().main(new String[] {args[1]});
             }
             break;
             case "--search":{
-                System.out.println("Searching");
+                logger.info("Searching.");
                 TigerGeocoder tigerGeocoder = new TigerGeocoder();
                 tigerGeocoder.init();
                 tigerGeocoder.setIndexDirectory(args[1]);
                 String queryAddress = args[2];
-                System.out.println(tigerGeocoder.search(queryAddress));
+                logger.info(tigerGeocoder.search(queryAddress));
             }
             break;
             case "--batch":{
-                System.out.println("Batch Geocoding");
+                logger.info("Batch Geocoding");
                 BatchGeocoder.main(new String[]{args[1], args[2], args[3]});
             }
             break;
