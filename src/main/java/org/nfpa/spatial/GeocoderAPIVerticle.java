6b0a4c4f3af65d5f55b7a21cd8f241ffcb4437d1
rahul-pande <93rahulpande@gmail.com>
2019-10-25 15:02:41 +0000

    api initial commit

---
+++ src/main/java/org/nfpa/spatial/GeocoderAPIVerticle.java
@@ -0,0 +1,83 @@
+package org.nfpa.spatial;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParser;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Route;
+import io.vertx.ext.web.Router;
+import org.apache.log4j.Logger;
+import org.apache.wink.json4j.JSONArray;
+import org.apache.wink.json4j.JSONException;
+import org.apache.wink.json4j.JSONObject;
+import org.apache.wink.json4j.OrderedJSONObject;
+import scala.collection.immutable.Map;
+
+import java.io.IOException;
+
+public class GeocoderAPIVerticle extends AbstractVerticle {
+
+    private GeocodeWrapper tigerGeocoder;
+    private JSONObject errorResponse;
+    private String address;
+    private Integer numRes;
+    private OrderedJSONObject result;
+    private JSONArray results;
+    private static Logger logger = Logger.getLogger(GeocoderAPIVerticle.class);
+
+    private void init() throws IOException, JSONException {
+        tigerGeocoder = new GeocodeWrapper(config().getString("index.dir"));
+        errorResponse = new JSONObject();
+
+        result = new OrderedJSONObject();
+        result.put("version", config().getString("api.version"));
+    }
+
+    @Override
+    public void start(Future<Void> fut) throws IOException, JSONException {
+        init();
+
+        HttpServer server = vertx.createHttpServer();
+
+        Router router = Router.router(vertx);
+        Route route = router.route().path("/geocoder/v1/");
+
+        route.handler(routingContext -> {
+            address = routingContext.request().getParam("address");
+            numRes = Integer.parseInt(routingContext.request().getParam("n"));
+            logger.info("address: " + address + " | " + "number of results: " + numRes);
+
+            HttpServerResponse response = routingContext.response();
+            response.putHeader("content-type", "application/json");
+
+            try {
+                results = tigerGeocoder.getSearchJSONArray(address, numRes);
+                result.put("input", address);
+                result.put("results", results);
+
+                response.end(
+                        result.toString(4)
+                );
+            } catch(Exception e){
+                try {
+                    errorResponse.put("error", e.toString());
+                } catch (JSONException ex) {
+                    ex.printStackTrace();
+                }
+                response.end(
+                        errorResponse.toString()
+                );
+            }
+
+        });
+
+        server.requestHandler(router).listen(
+                config().getInteger("http.port", 8080)
+        );
+    }
+}
