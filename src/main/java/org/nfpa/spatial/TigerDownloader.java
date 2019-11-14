30f7053c68731a39d0b42a2cf92ec1e5e81b5a77
rahul-pande <93rahulpande@gmail.com>
2019-11-14 19:41:47 +0000

    filter based on state code and unzip

--- src/main/java/org/nfpa/spatial/TigerDownloader.java
+++ src/main/java/org/nfpa/spatial/TigerDownloader.java
@@ -1,25 +1,43 @@
 package org.nfpa.spatial;
 
-import org.apache.commons.net.ftp.FTPClient;
-import org.apache.commons.net.ftp.FTPClientConfig;
-import org.apache.commons.net.ftp.FTPReply;
+import org.apache.commons.net.ftp.*;
 import org.apache.log4j.Logger;
+import org.nfpa.spatial.utils.StateFIPS;
+import org.nfpa.spatial.utils.ZipUtils;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 public class TigerDownloader {
     private FTPClient ftp;
     private FTPClientConfig ftpConfig;
     private Properties properties;
+    private HashSet<String> statesFIPS = new HashSet<String>();
     private static Logger logger = Logger.getLogger(TigerDownloader.class);
+
+    private static String TIGER_DOWNLOAD_DIR;
+    private static String TIGER_BASE_DIR;
+    private static String TIGER_FTP;
+    private static String TIGER_STATES;
+    private static String TIGER_TYPES;
+    private static String TIGER_FILTER_TYPES;
 
     private void readProperties() throws IOException {
         InputStream in = this.getClass().getClassLoader().getResourceAsStream("tiger.properties");
         properties = new Properties();
         properties.load(in);
+
+        TIGER_DOWNLOAD_DIR = properties.getProperty("tiger.download.dir");
+        TIGER_BASE_DIR = properties.getProperty("tiger.base.dir");
+        TIGER_FTP = properties.getProperty("tiger.ftp");
+        TIGER_STATES = properties.getProperty("tiger.states");
+        TIGER_TYPES = properties.getProperty("tiger.types");
+        TIGER_FILTER_TYPES = properties.getProperty("tiger.filter.types");
+
+        for (String state : TIGER_STATES.split(",")){
+            statesFIPS.add(StateFIPS.getFIPS(state.trim()));
+        }
+        logger.info(statesFIPS);
     }
 
     private void logResponse(FTPClient ftp) throws IOException {
@@ -37,10 +55,11 @@ public class TigerDownloader {
     private void initFTPClient() throws IOException {
         ftp = new FTPClient();
         ftp.configure(ftpConfig);
-        ftp.connect(properties.getProperty("tiger.ftp"));
+        ftp.connect(TIGER_FTP);
         ftp.setControlKeepAliveTimeout(300);
         ftp.enterLocalPassiveMode();
         ftp.login("anonymous", null);
+        ftp.setFileType(FTP.BINARY_FILE_TYPE);
 
         logResponse(ftp);
     }
@@ -50,33 +69,85 @@ public class TigerDownloader {
     }
 
     private void downloadData() throws IOException {
-        new File(properties.getProperty("tiger.download.dir")).mkdirs();
+        new File(TIGER_DOWNLOAD_DIR).mkdirs();
 
-        List<String> subDirectories = getDownloadDirectories();
+        HashSet<String> subDirectories = getDownloadDirectories();
+        HashSet<String> filterDirectories = getFilterDirectories();
+
+        logger.info("All files directories: " + subDirectories);
+        logger.info("Filter files directories: " +filterDirectories);
+
         for (String directory: subDirectories){
-            ftp.changeWorkingDirectory(properties.getProperty("tiger.base.dir"));
+            ftp.changeWorkingDirectory(TIGER_BASE_DIR);
             ftp.changeWorkingDirectory(directory);
-            List<String> files = Arrays.asList(ftp.listNames());
-            logger.info(files);
+            FTPFile[] files = ftp.listFiles();
 
-            for (String file: files){
-                downloadRemoteFile(file);
+            for (FTPFile file: files){
+                String fileName = file.getName();
+                if (filterDirectories.contains(directory)){
+                    if (isValidStateFile(fileName)){
+                        logger.info("DOWNLOADING: " + fileName + " SIZE: " + file.getSize()/1024 + "KB");
+                        downloadRemoteFile(fileName, directory);
+                        logger.info("Unzipping: " + fileName);
+                        unzipFile(fileName, directory);
+                    }
+                }
+                else {
+                    logger.info("DOWNLOADING: " + fileName + " SIZE: " + file.getSize()/1024 + "KB");
+                    downloadRemoteFile(fileName, directory);
+                    logger.info("Unzipping: " + fileName);
+                    unzipFile(fileName, directory);
+                }
             }
         }
     }
 
-    private void downloadRemoteFile(String remoteName) throws IOException {
-        String localFile = properties.getProperty("tiger.download.dir") +
-                "/" + remoteName;
+    private boolean isValidStateFile(String remoteFileName){
+        String fileFIPS = remoteFileName.split("_")[2].substring(0,2);
+        return statesFIPS.contains(fileFIPS);
+    }
+
+    private void downloadRemoteFile(String remoteName, String subDirectory) throws IOException {
+        String typeDir = subDirectory.toLowerCase();
+        new File(TIGER_DOWNLOAD_DIR + "/" + typeDir).mkdirs();
+        String localFile = TIGER_DOWNLOAD_DIR +
+                "/" + typeDir + "/" + remoteName;
+
         FileOutputStream fos = new FileOutputStream(localFile);
         ftp.retrieveFile(remoteName, fos);
         fos.close();
         logger.info("COMPLETE: " + remoteName);
     }
 
-    private List<String> getDownloadDirectories(){
-        String[] subdirectories = properties.getProperty("tiger.types").split(",");
-        return Arrays.asList(subdirectories);
+    private void unzipFile(String remoteName, String subDirectory) {
+        String typeDir = subDirectory.toLowerCase();
+        String localFile = TIGER_DOWNLOAD_DIR +
+                "/" + typeDir + "/" + remoteName;
+        ZipUtils.unzip(localFile,
+                TIGER_DOWNLOAD_DIR +
+                        "/" +
+                        "uncompressed" +
+                        "/" +
+                        typeDir +
+                        "/" +
+                        remoteName.split(".zip")[0]
+        );
+    }
+
+    private HashSet<String> getDownloadDirectories(){
+        HashSet<String> subDirectories = new HashSet<String>();
+        for (String directory : TIGER_TYPES.split(",")){
+            subDirectories.add(directory.trim());
+        };
+        return subDirectories;
+    }
+
+    private HashSet<String> getFilterDirectories(){
+        HashSet<String> filterDirectories = new HashSet<String>();
+        for (String directory : TIGER_FILTER_TYPES.split(",")){
+            filterDirectories.add(directory.trim());
+        };
+        return filterDirectories;
     }
 
     public static void main(String[] args) throws IOException {
---
+++ src/main/java/org/nfpa/spatial/utils/ZipUtils.java
@@ -0,0 +1,24 @@
+package org.nfpa.spatial.utils;
+
+import net.lingala.zip4j.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+import org.apache.log4j.Logger;
+
+public class ZipUtils {
+    private static Logger logger = Logger.getLogger(ZipUtils.class);
+    public static boolean unzip(String source, String destination){
+
+        try {
+            ZipFile zipFile = new ZipFile(source);
+            if (zipFile.isEncrypted()) {
+                logger.info("File is encrypted");
+                return false;
+            }
+            zipFile.extractAll(destination);
+            return true;
+        } catch (ZipException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
