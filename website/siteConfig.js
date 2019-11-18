3c0f4581361e06ea4b8ada9232a6811a69f0a361
Mohammed Ayub <mohammedayub44@gmail.com>
2019-11-18 10:01:32 -0500

    Changed LF to CRLF in files
    git config changes

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'Moose Geocoder', // Title for your website.
   tagline: 'Geocoder Faster than Before',
   url: 'https://NFPA.github.io', // Your website URL
-  baseUrl: '/NFPA/LocationTools/', // Base URL for your project */
+  baseUrl: '/LocationTools/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
